8ef0fd36184afe7b0ec1ab23960692a2a8205539
ella gaul <ellagaul@ellas-MacBook-Pro.local>
2024-04-14 03:17:08 +0400

    set up vite and add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Header.css'
+const Header = () => {
+  return (
+    <header>
+      <h1 className='header'>Elizabeth Gaul</h1>
+    </header>
+  );
+};
+
+export default Header;
